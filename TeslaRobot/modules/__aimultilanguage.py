bc6ecd351c60efd2d7ce88410fa693558b66dc64
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-06-30 15:14:56 +0530

    Initial Commit

--- TeslaRobot/modules/__aimultilanguage.py
+++ TeslaRobot/modules/__aimultilanguage.py
@@ -1,6 +1,6 @@
 #    Copyright (C) 2020-2021 by @AmarnathCdj & @InukaAsith
 #    Chatbot system written by @AmarnathCdj databse added and recoded for pyrogram by @InukaAsith
-#    This programme is a part of shasa (TG bot) project
+#    This programme is a part of TeslaRobo (TG bot) project
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -27,7 +27,7 @@ from pyrogram import filters
 
 from TeslaRobot.helper_extra.aichat import add_chat, get_session, remove_chat
 from TeslaRobot.pyrogramee.pluginshelper import admins_only, edit_or_reply
-from TeslaRobot import pbot as shasa
+from TeslaRobot import pbot as tesla
 
 translator = google_translator()
 
@@ -35,13 +35,13 @@ translator = google_translator()
 def extract_emojis(s):
     return "".join(c for c in s if c in emoji.UNICODE_EMOJI)
 
-BOT_ID = 1791708933
-shasa_chats = []
+BOT_ID = 1811984200
+tesla_chats = []
 en_chats = []
 # AI Chat (C) 2020-2021 by @InukaAsith
 
 
-@shasa.on_message(filters.command("chatbot") & ~filters.edited & ~filters.bot)
+@tesla.on_message(filters.command("chatbot") & ~filters.edited & ~filters.bot)
 @admins_only
 async def hmm(_, message):
     global asuna_chats
@@ -56,20 +56,20 @@ async def hmm(_, message):
         lel = await edit_or_reply(message, "`Processing...`")
         lol = add_chat(int(message.chat.id))
         if not lol:
-            await lel.edit("shasa AI Already Activated In This Chat")
+            await lel.edit("TeslaRobo AI Already Activated In This Chat")
             return
         await lel.edit(
-            f"shasa AI Successfully Added For Users In The Chat {message.chat.id}"
+            f"TeslaRobo AI Successfully Added For Users In The Chat {message.chat.id}"
         )
 
     elif status == "OFF" or status == "off" or status == "Off":
         lel = await edit_or_reply(message, "`Processing...`")
         Escobar = remove_chat(int(message.chat.id))
         if not Escobar:
-            await lel.edit("shasa AI Was Not Activated In This Chat")
+            await lel.edit("TeslaRobo AI Was Not Activated In This Chat")
             return
         await lel.edit(
-            f"shasa AI Successfully Deactivated For Users In The Chat {message.chat.id}"
+            f"TeslaRobo AI Successfully Deactivated For Users In The Chat {message.chat.id}"
         )
 
     elif status == "EN" or status == "en" or status == "english":
@@ -85,7 +85,7 @@ async def hmm(_, message):
         )
 
 
-@shasa.on_message(
+@tesla.on_message(
     filters.text & filters.reply & ~filters.bot & ~filters.via_bot & ~filters.forwarded,
     group=2,
 )
@@ -98,8 +98,8 @@ async def hmm(client, message):
         message.continue_propagation()
     if chat_id in en_chats:
         test = msg
-        test = test.replace("shasa", "Aco")
-        test = test.replace("shasa", "Aco")
+        test = test.replace("tesla", "Aco")
+        test = test.replace("tesla", "Aco")
         querystring = {
             "bid": "178",
             "key": "sX5A2PcYZbsN5EY6",
@@ -114,12 +114,12 @@ async def hmm(client, message):
         result = response.text
         result = result.replace('{"cnt":"', "")
         result = result.replace('"}', "")
-        result = result.replace("Aco", "shasa")
+        result = result.replace("Aco", "tesla")
         result = result.replace("<a href=\\", "<a href =")
         result = result.replace("<\/a>", "</a>")
         pro = result
         try:
-            await shasa.send_chat_action(message.chat.id, "typing")
+            await tesla.send_chat_action(message.chat.id, "typing")
             await message.reply_text(pro)
         except CFError as e:
             print(e)
@@ -160,8 +160,8 @@ async def hmm(client, message):
         # test = emoji.demojize(test.strip())
 
         # Kang with the credits bitches @InukaASiTH
-        test = test.replace("shasa", "Aco")
-        test = test.replace("shasa", "Aco")
+        test = test.replace("tesla", "Aco")
+        test = test.replace("tesla", "Aco")
         querystring = {
             "bid": "178",
             "key": "sX5A2PcYZbsN5EY6",
@@ -176,20 +176,20 @@ async def hmm(client, message):
         result = response.text
         result = result.replace('{"cnt":"', "")
         result = result.replace('"}', "")
-        result = result.replace("Aco", "shasa")
+        result = result.replace("Aco", "tesla")
         result = result.replace("<a href=\\", "<a href =")
         result = result.replace("<\/a>", "</a>")
         pro = result
         if not "en" in lan and not lan == "":
             pro = translator.translate(pro, lang_tgt=lan[0])
         try:
-            await shasa.send_chat_action(message.chat.id, "typing")
+            await tesla.send_chat_action(message.chat.id, "typing")
             await message.reply_text(pro)
         except CFError as e:
             print(e)
 
 
-@shasa.on_message(filters.text & filters.private & filters.reply & ~filters.bot)
+@tesla.on_message(filters.text & filters.private & filters.reply & ~filters.bot)
 async def inuka(client, message):
     msg = message.text
     if msg.startswith("/") or msg.startswith("@"):
@@ -230,8 +230,8 @@ async def inuka(client, message):
     # test = emoji.demojize(test.strip())
 
     # Kang with the credits bitches @InukaASiTH
-    test = test.replace("shasa", "Aco")
-    test = test.replace("shasa", "Aco")
+    test = test.replace("tesla", "Aco")
+    test = test.replace("tesla", "Aco")
     querystring = {
         "bid": "178",
         "key": "sX5A2PcYZbsN5EY6",
@@ -246,21 +246,21 @@ async def inuka(client, message):
     result = response.text
     result = result.replace('{"cnt":"', "")
     result = result.replace('"}', "")
-    result = result.replace("Aco", "shasa")
+    result = result.replace("Aco", "tesla")
     result = result.replace("<a href=\\", "<a href =")
     result = result.replace("<\/a>", "</a>")
     pro = result
     if not "en" in lan and not lan == "":
         pro = translator.translate(pro, lang_tgt=lan[0])
     try:
-        await shasa.send_chat_action(message.chat.id, "typing")
+        await tesla.send_chat_action(message.chat.id, "typing")
         await message.reply_text(pro)
     except CFError as e:
         print(e)
 
 
-@shasa.on_message(
-    filters.regex("shasa|Shasa|huntinbots|hello|hi")
+@tesla.on_message(
+    filters.regex("tesla|Tesla|huntinbots|hello|hi")
     & ~filters.bot
     & ~filters.via_bot
     & ~filters.forwarded
@@ -308,8 +308,8 @@ async def inuka(client, message):
     # test = emoji.demojize(test.strip())
 
     # Kang with the credits bitches @InukaASiTH
-    test = test.replace("shasa", "Aco")
-    test = test.replace("shasa", "Aco")
+    test = test.replace("tesla", "Aco")
+    test = test.replace("tesla", "Aco")
     querystring = {
         "bid": "178",
         "key": "sX5A2PcYZbsN5EY6",
@@ -324,14 +324,14 @@ async def inuka(client, message):
     result = response.text
     result = result.replace('{"cnt":"', "")
     result = result.replace('"}', "")
-    result = result.replace("Aco", "shasa")
+    result = result.replace("Aco", "tesla")
     result = result.replace("<a href=\\", "<a href =")
     result = result.replace("<\/a>", "</a>")
     pro = result
     if not "en" in lan and not lan == "":
         pro = translator.translate(pro, lang_tgt=lan[0])
     try:
-        await shasa.send_chat_action(message.chat.id, "typing")
+        await tesla.send_chat_action(message.chat.id, "typing")
         await message.reply_text(pro)
     except CFError as e:
         print(e)
