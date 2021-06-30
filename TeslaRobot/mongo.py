099864484c7d767ac080ff8a7758d66530f2a69e
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-06-30 10:31:26 +0530

    Initial Commit

--- TeslaRobot/mongo.py
+++ TeslaRobot/mongo.py
@@ -1,25 +1,25 @@
-#Kiitu
 import asyncio
 import sys
 
 from motor import motor_asyncio
-from ShasaBot import MONGO_DB_URI 
+from TeslaRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from ShasaBot.conf import get_int_key, get_str_key
+from TeslaRobot.conf import get_int_key, get_str_key
+from TeslaRobot import LOGGER
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "DaisyX"
+MONGO_DB = "TeslaRobot"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["ShasaBot"]
+db = client["TeslaRobo"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
-    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
+    sys.exit(LOGGER.critical("Can't connect to Mongo DB! Exiting..."))
