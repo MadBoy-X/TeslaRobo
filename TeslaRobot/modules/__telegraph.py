41718986e73894b061e05b9e2b3c23a6bd7f824b
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-06-30 15:42:16 +0530

    Initial Commit

--- TeslaRobot/modules/__telegraph.py
+++ TeslaRobot/modules/__telegraph.py
@@ -6,9 +6,9 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-mdnoor = "SHASA"
+madboy = "TESLA"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=mdnoor)
+r = telegraph.create_account(short_name=madboy)
 auth_url = r["auth_url"]
 
 
