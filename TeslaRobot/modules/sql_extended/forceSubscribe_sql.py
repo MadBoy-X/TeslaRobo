92dd22f9ca38e3c0726853c84925a39d1ef432ef
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-06-30 12:29:44 +0530

    Initial Commit

--- TeslaRobot/modules/sql_extended/forceSubscribe_sql.py
+++ TeslaRobot/modules/sql_extended/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from ShasaBot.modules.sql import BASE, SESSION
+from TeslaRobot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
