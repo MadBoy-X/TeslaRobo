951ff28548b47a5f6d4a1c3cd3547c560e41a0ca
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-06-30 17:17:28 +0530

    Initial Commit

--- TeslaRobot/modules/helper_funcs/misc.py
+++ TeslaRobot/modules/helper_funcs/misc.py
@@ -69,7 +69,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("👈🏻", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("«« 𝘽𝙖𝙘𝙠", callback_data="tesla_back"),
+                EqInlineKeyboardButton("𝙃𝙤𝙢𝙚 🏠", callback_data="tesla_back"),
              EqInlineKeyboardButton("👉🏻", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
